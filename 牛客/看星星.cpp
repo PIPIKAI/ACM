6ea3b859edf2e60e4a079c919b724f1270f2be6d
kiass <1652091948@qq.com>
2019-08-07 17:26:22 +0800

    111

--- 牛客/看星星.cpp
+++ 牛客/看星星.cpp
@@ -1,9 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int maxn=999;
-int f[maxn];
+int f[maxn],k[maxn],x,y,ans;
+bool vis[]
+void dfs(int xi,int yi,int step)
+{
+
+}
 int main()
 {
-    
+    cin>>x>>y;
+    for(int i=0;i<x;i++){
+        cin>>f[i];
+    }
+    for(int i=0;i<y;i++){
+        cin>>k[i];
+    }
+    dfs(0,0,0);
+    cout<<x+y-ans<<endl;
     return 0;
 }
