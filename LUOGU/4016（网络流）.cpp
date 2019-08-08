6581d83780544944913b1c5a899d0e7baf6fd079
kiass <1652091948@qq.com>
2019-08-08 16:30:44 +0800

    tip

---
+++ LUOGU/4016（网络流）.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+#define Max(x,y) y>x?x=y:x=x
+#define mem(a,b)  memset( a,b,sizeof a)
+typedef long long ll;
+const int inf=0x7fffffff;
+const int maxn = 1e5+7;
+int n,m;
+int f[maxn];
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int T,Case=0;
+    cin>>T;
+    while(T--){
+        
+        cout<<"Case "<<++Case<<": ";
+        cout<<endl;
+    }
+    return 0;
+}
